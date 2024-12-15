1320f2e7eae221eca490596076719265feb634f0
Tae young Kim <tyk0707@gmail.com>
2024-12-16 00:56:49 +0900

    1200

--- todo/tailwind.config.ts
+++ todo/tailwind.config.ts
@@ -1,18 +1,18 @@
 import type { Config } from "tailwindcss";
+import withMT from "@material-tailwind/react/utils/withMT";
+import typography from "@tailwindcss/typography";
 
-export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-} satisfies Config;
+const config: Config = { 
+    content: [
+        "./utils/**/*.{js,ts,jsx,tsx,mdx}",
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {},
+    plugins: [
+        typography
+    ],
+};
+
+export default withMT(config);
