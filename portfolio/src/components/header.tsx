858857f56beaa3e7adb10313e47c19fc0f5cf492
Tae young Kim <tyk0707@gmail.com>
2024-12-24 00:15:00 +0900

    1200

--- portfolio/src/components/header.tsx
+++ portfolio/src/components/header.tsx
@@ -7,17 +7,6 @@ import Link from 'next/link';
 import { BsSunFill } from 'react-icons/bs';
 
 // 다크 모드 전환 버튼 // 
-// function mode(theme ?: string) {
-//     return (
-//         theme === 'dark' ? 
-//         (
-            
-//         ) : 
-//         (
-            
-//         )
-//     )
-// };
 
 export default function Header() {
     const { theme, setTheme } = useTheme(); 
@@ -29,15 +18,15 @@ export default function Header() {
         <>
         <header className="text-gray-600 body-font">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-                <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-                    <svg 
-                        xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" 
-                        strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" 
-                        className="w-10 h-10 text-white p-2 bg-blue-500 rounded-full" viewBox="0 0 24 24">
-                        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-                    </svg>
-                    <span className="ml-3 text-xl">태영의 포트폴리오</span>
-                </a>
+                    <div className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                        <svg 
+                            xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" 
+                            strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" 
+                            className="w-10 h-10 text-white p-2 bg-blue-500 rounded-full" viewBox="0 0 24 24">
+                            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+                        </svg>
+                            <Link className="ml-3 text-xl" href={'/'}>태영의 포트폴리오</Link>
+                    </div>
                 <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
                     <Link className="mr-5 hover:text-gray-900 dark:hover:text-slate-50" href={'/'}>홈</Link>
                     <Link className="mr-5 hover:text-gray-900 dark:hover:text-slate-50" href={'/about-me'}>소개</Link>
