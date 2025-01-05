ed908e4e1dcf4527e5155e22f36e3d76a5c42653
Tae young Kim <tyk0707@gmail.com>
2025-01-05 23:55:26 +0900

    0000

--- dropbox/src/components/file-dragdropzone.tsx
+++ dropbox/src/components/file-dragdropzone.tsx
@@ -1,14 +1,27 @@
 
 'use client';
 
+import React, {useCallback} from 'react'
+import {useDropzone} from 'react-dropzone'
+
 export default function FileDragDropZone() {
+
+    const onDrop = useCallback(acceptedFiles => {
+        // Do something with the files
+      }, [])
+      const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
     return (
-        <section className="w-2/6 py-20 border-2 border-dotted border-indigo-700 flex flex-col rounded-xl items-center justify-center">
+        <div
+            className="w-2/6 py-20 border-2 border-dotted border-indigo-700 flex flex-col rounded-xl items-center justify-center" 
+            {...getRootProps()}>
             <input 
-                type="file"/>
-            <p>
-                파일을 여기에 끌어다 놓거나 클릭하여 업로드 하세요.
-            </p>
-        </section>
+                {...getInputProps()} />
+            {
+                isDragActive ?
+                <p>Drop the files here ...</p> :
+                <p>파일을 여기에 끌어다 놓거나 클릭하여 업로드 하세요.</p>
+            }
+        </div>
     );
 };
---
+++ netflix/tailwind.config.ts
@@ -0,0 +1,24 @@
+
+import type { Config } from "tailwindcss";
+import withMT from "@material-tailwind/react/utils/withMT";
+
+export default withMT ({
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./utils/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+} satisfies Config );
